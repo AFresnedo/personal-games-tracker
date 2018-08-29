4ec3d36af32a756efe8ebc209cfa808be6ac79f9
AFresnedo <AFresnedo@users.noreply.github.com>
2018-08-28 17:10:53 -0700

    create played_games_to_games join table

--- migrations/20180828235845-played_games-games-join-table.js
+++ migrations/20180828235845-played_games-games-join-table.js
@@ -1,23 +1,31 @@
 'use strict';
-
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
+    return queryInterface.createTable('played_games_to_games', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      played_gameId: {
+        type: Sequelize.INTEGER
+      },
+      gameId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
   },
-
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
+    return queryInterface.dropTable('played_games_to_games');
   }
 };
+;
