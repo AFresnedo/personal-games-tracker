f67c0c837f88ccb58303c2b6f1f8dc2c9ac76373
AFresnedo <AFresnedo@users.noreply.github.com>
2018-09-03 20:33:37 -0700

    create seeding script

---
+++ seeds.sh
@@ -0,0 +1,6 @@
+sequelize db:seed:undo --seed seeders/played_games.js
+sequelize db:seed:undo --seed seeders/wishes.js
+sequelize db:seed:undo --seed seeders/users.js
+sequelize db:seed --seed seeders/users.js
+sequelize db:seed --seed seeders/played_games.js
+sequelize db:seed --seed seeders/wishes.js
