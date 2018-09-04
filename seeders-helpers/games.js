b7b70ae0b0a8c0e18bc3ca09b4edab14f8660e95
AFresnedo <AFresnedo@users.noreply.github.com>
2018-09-03 17:25:55 -0700

    code recursive function for appending real game names to gamelist

--- seeders-helpers/games.js
+++ seeders-helpers/games.js
@@ -5,21 +5,34 @@ require('dotenv').config();
 const igdb = require('igdb-api-node').default;
 const client = igdb(process.env.IGDB_API_KEY);
 
-// data structure for real game names
+// list of real game names
+let gamesList = [];
 
-// returns an api response for 50 "random" game names
-function getRandFifty() {
-  // refer to https://github.com/igdb/igdb-api-node
-  client.games({
-    fields: '*',
-    limit: 50,
-    offset: Math.floor(Math.random() * 100) + Math.floor(Math.random() * 10)
-  }, [ 'name' ]).then( res => {
-    console.log(res);
-  }).catch( err => {
-    console.log(err);
-  });
+// adds 50 random game titles "times" times
+function getRandFifty(callsRemaining) {
+  // count as "one more time"
+  if (callsRemaining <= 0) {
+    // done
+    // TODO call seeder
+  }
+  else {
+    // add 50 more game titles to the list
+    // refer to https://github.com/igdb/igdb-api-node
+    client.games({
+      fields: '*',
+      limit: 50,
+      offset: Math.floor(Math.random() * 100) + Math.floor(Math.random() * 10)
+    }, [ 'name' ]).then( res => {
+      // res.body is an array of objects with the "id" and "name" keys
+      res.body.forEach( (obj) => {
+        gamesList.push(obj.name);
+      });
+      console.log(gamesList);
+      getRandFifty(callsRemaining - 1);
+    }).catch( err => {
+      console.log(err);
+    });
+  }
 }
 
-getRandFifty();
-getRandFifty();
+getRandFifty(2);
