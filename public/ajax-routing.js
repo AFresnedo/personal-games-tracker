6383c87aecc16b76c8b1c1146829da29a20cfde2
AFresnedo <AFresnedo@users.noreply.github.com>
2018-09-06 11:59:19 -0700

    write ajax routing file

---
+++ public/ajax-routing.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+  console.log('ajax-routing loaded on this page');
+
+  $('.delete-wish').click(function(e) {
+    e.preventDefault();
+    console.log('delete requested for:', $(this).attr('href'));
+
+    $.ajax({
+      url: $(this).attr('href'),
+      method: 'DELETE'
+    }).done(function(msg) {
+      console.log('success msg:', msg);
+      window.location = '/wishes/:id';
+    }).fail(function(msg) {
+      console.log('err msg:', msg);
+    });
+  });
+
+  // $('#edit-tag').submit(function(e) {
+    // e.preventDefault();
+    // console.log('recieved edit form');
+
+    // // serialize takes all form data and sends by name
+    // $.ajax({
+      // url: $(this).attr('action'),
+      // method: 'PUT',
+      // data: $(this).serialize()
+    // }).done(function(msg) {
+      // console.log('success msg:', msg);
+    // }).fail(function(msg) {
+      // console.log('fail msg:', msg);
+    // })
+  // });
+});
